4af31e64bcc4d4383135767fb37d3aaa846f7202
usuario <usuario@L-1706039>
2017-08-21 16:49:22 +0200

    Modificado nombre interfaz IPersonasRepository

--- ServidorDatosBancarios/Repository/IPersonasRepository.cs
+++ ServidorDatosBancarios/Repository/IPersonasRepository.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ServidorDatosBancarios.Repository {
-    public interface IPersonasRepositorio {
+    public interface IPersonasRepository {
 
     }
 }
